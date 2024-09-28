767ff76ebd7179f404839c7b76b64db50c550eaf
NahuelLazarte <clazarte@frba.utn.edu.ar>
2024-09-28 12:31:33 -0300

    Agrego skybox para el fondo. Hay que corregir la cámara

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Numerics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using TGC.MonoGame.TP.Modelos;
+using TGC.MonoGame.TP.Fondo;
 
 using Vector3 = Microsoft.Xna.Framework.Vector3;
 
 namespace TGC.MonoGame.TP
 {
-    /// <summary>
-    ///     Esta es la clase principal del juego.
-    ///     Inicialmente puede ser renombrado o copiado para hacer mas ejemplos chicos, en el caso de copiar para que se
-    ///     ejecute el nuevo ejemplo deben cambiar la clase que ejecuta Program <see cref="Program.Main()" /> linea 10.
-    /// </summary>
     public class TGCGame : Game
     {
         public const string ContentFolder3D = "Models/";
@@ -26,21 +20,12 @@ namespace TGC.MonoGame.TP
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
         public const string ContentFolderTextures = "Textures/";
 
-        /// <summary>
-        ///     Constructor del juego.
-        /// </summary>
         public TGCGame()
         {
-            // Maneja la configuracion y la administracion del dispositivo grafico.
             Graphics = new GraphicsDeviceManager(this);
-            
             Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
             Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
-            
-            // Para que el juego sea pantalla completa se puede usar Graphics IsFullScreen.
-            // Carpeta raiz donde va a estar toda la Media.
             Content.RootDirectory = "Content";
-            // Hace que el mouse sea visible.
             IsMouseVisible = true;
         }
 
@@ -49,62 +34,53 @@ namespace TGC.MonoGame.TP
         private Model Model { get; set; }
         private Effect Effect { get; set; }
 
-
         private Matrix World { get; set; }
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
-        private VertexBuffer VertexBuffer{ get; set;}
-        private IndexBuffer IndexBuffer{ get; set; }
+        private VertexBuffer VertexBuffer { get; set; }
+        private IndexBuffer IndexBuffer { get; set; }
 
-        private Sphere esfera{ get; set; }
+        private Sphere esfera { get; set; }
 
-        private List<Car> CarList{ get; set; }
+        private List<Car> CarList { get; set; }
         private const uint NumCars = 50;
-        private List<Tree> TreeList{ get; set; }
+        private List<Tree> TreeList { get; set; }
         private const uint NumTree = 50;
-        private List<Tunnel> TunnelList{ get; set; }
+        private List<Tunnel> TunnelList { get; set; }
         private const uint NumTunnel = 10;
-        private List<Wave_A> Wave_AList{ get; set; }
+        private List<Wave_A> Wave_AList { get; set; }
         private const uint NumWave_A = 20;
-        private const float MapSize = 500f;       
+        private const float MapSize = 500f;
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aqui el codigo de inicializacion: el procesamiento que podemos pre calcular para nuestro juego.
-        /// </summary>
+        private SkyBox SkyBox { get; set; }
+        private Vector3 CameraPosition { get; set; }
+        private float Distance { get; set; }
+        private Vector3 CameraTarget { get; set; }
+        private Vector3 ViewVector { get; set; }
+        private float Angle { get; set; }
+
         protected override void Initialize()
         {
-            // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
-
-           // Configuramos nuestras matrices de la escena.           
-
             World = Matrix.Identity;
-            View = Matrix.CreateLookAt(new Vector3(0f, 25f, 100f), Vector3.Zero, Vector3.Up);
-            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 0.1f, 1000f);
 
             CarList = new List<Car>();
             TreeList = new List<Tree>();
             TunnelList = new List<Tunnel>();
             Wave_AList = new List<Wave_A>();
 
+            CameraTarget = new Vector3(-10f, 0f, -10f);//Vector3.Zero;
+            Distance = 400;
+
             base.Initialize();
         }
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
-        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
-        ///     que podemos pre calcular para nuestro juego.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // Cargo un efecto basico propio declarado en el Content pipeline.
-            // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
             Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
 
-            esfera = new Sphere( Content, new Vector3(0f, 3.4f, 0f), Matrix.Identity, Color.Purple );
+            esfera = new Sphere(Content, new Vector3(0f, 3.4f, 0f), Matrix.Identity, Color.Purple);
             esfera.LoadContent(Effect);
 
             AgregarTunnel(NumTunnel);
@@ -123,164 +99,152 @@ namespace TGC.MonoGame.TP
             VertexBuffer.SetData(vertices);
 
             IndexBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 6, BufferUsage.None);
-            var indices = new ushort[]{ 0, 1, 2, 1, 3, 2};
-
+            var indices = new ushort[] { 0, 1, 2, 1, 3, 2 };
             IndexBuffer.SetData(indices);
 
-            
+            var skyBox = Content.Load<Model>("Models/skybox/cube");
+            var skyBoxTexture = Content.Load<TextureCube>(ContentFolderTextures + "/skybox/skybox");
+            var skyBoxEffect = Content.Load<Effect>(ContentFolderEffects + "SkyBox");
+            SkyBox = new SkyBox(skyBox, skyBoxTexture, skyBoxEffect);
 
             base.LoadContent();
         }
 
-        /// <summary>
-        ///     Se llama en cada frame.
-        ///     Se debe escribir toda la logica de computo del modelo, asi como tambien verificar entradas del usuario y reacciones
-        ///     ante ellas.
-        /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            // Aca deberiamos poner toda la logica de actualizacion del juego.
-
-            // Capturar Input teclado
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
-                //Salgo del juego.
                 Exit();
             }
-            
 
-            // Basado en el tiempo que paso se va generando una rotacion.
+            CameraPosition = Distance * new Vector3((float)Math.Sin(Angle), 0, (float)Math.Cos(Angle));
+            ViewVector = Vector3.Transform(CameraTarget - CameraPosition, Matrix.CreateRotationY(0));
+            ViewVector.Normalize();
+
+            Angle += 0.002f;
+            View = Matrix.CreateLookAt(CameraPosition, CameraTarget, Vector3.UnitY);
+
             base.Update(gameTime);
         }
 
-        /// <summary>
-        ///     Se llama cada vez que hay que refrescar la pantalla.
-        ///     Escribir aqui el codigo referido al renderizado.
-        /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Aca deberiamos poner toda la logia de renderizado del juego.
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            Effect.CurrentTechnique = Effect.Techniques["BasicColorDrawing"];
 
-            var rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
+            var originalRasterizerState = GraphicsDevice.RasterizerState;
+            var rasterizerState = new RasterizerState
+            {
+                CullMode = CullMode.None
+            };
             GraphicsDevice.RasterizerState = rasterizerState;
 
+            SkyBox.Draw(View, Projection, CameraPosition);
+
+            GraphicsDevice.RasterizerState = originalRasterizerState;
+
+            Effect.CurrentTechnique = Effect.Techniques["BasicColorDrawing"];
             Effect.Parameters["World"].SetValue(World);
             Effect.Parameters["View"].SetValue(View);
             Effect.Parameters["Projection"].SetValue(Projection);
             Effect.Parameters["DiffuseColor"].SetValue(Color.Gray.ToVector3());
-            
-            
+
             GraphicsDevice.Indices = IndexBuffer;
             GraphicsDevice.SetVertexBuffer(VertexBuffer);
-            
-            
-            foreach( var passes in Effect.CurrentTechnique.Passes )
+
+            foreach (var pass in Effect.CurrentTechnique.Passes)
             {
-                passes.Apply();
+                pass.Apply();
                 GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 2);
             }
-            
-            
-            foreach( Car _car in CarList)
+
+            foreach (var car in CarList)
             {
-                _car.Draw(Effect);
-            }
-            
-            foreach( Tree _tree in TreeList )
-            {
-                _tree.Draw(Effect);
+                car.Draw(Effect);
             }
 
-            foreach( Tunnel _tunnel in TunnelList )
+            foreach (var tree in TreeList)
             {
-                _tunnel.Draw(Effect);
+                tree.Draw(Effect);
             }
 
-            foreach( Wave_A _waveA in Wave_AList )
+            foreach (var tunnel in TunnelList)
             {
-                _waveA.Draw(Effect);
+                tunnel.Draw(Effect);
+            }
+
+            foreach (var waveA in Wave_AList)
+            {
+                waveA.Draw(Effect);
             }
 
             esfera.Draw(Effect);
 
+            base.Draw(gameTime);
         }
 
-        protected void AgregarAutos(uint _cantidad)
-        {   
-            int seed = 1;
-            Random _random = new Random(seed);
+        protected void AgregarAutos(uint cantidad)
+        {
+            var random = new Random(1);
 
-            for (uint i = 0; i < _cantidad; i++)
+            for (uint i = 0; i < cantidad; i++)
             {
-                float pos_x = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
-                float pos_z = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
+                float posX = (float)(random.NextDouble() * 2 * MapSize - MapSize);
+                float posZ = (float)(random.NextDouble() * 2 * MapSize - MapSize);
 
-                Car _newCar = new Car(Content, new Vector3(pos_x, 0f, pos_z), Matrix.CreateRotationY((float)(_random.NextDouble())), Color.Red);
-                _newCar.LoadContent(Effect);
-                CarList.Add(_newCar);
+                var newCar = new Car(Content, new Vector3(posX, 0f, posZ), Matrix.CreateRotationY((float)(random.NextDouble())), Color.Red);
+                newCar.LoadContent(Effect);
+                CarList.Add(newCar);
             }
         }
 
-         protected void AgregarArbol(uint _cantidad)
-        {   
-            int seed = 3;
-            Random _random = new Random(seed);
+        protected void AgregarArbol(uint cantidad)
+        {
+            var random = new Random(3);
 
-            for (uint i = 0; i < _cantidad; i++)
+            for (uint i = 0; i < cantidad; i++)
             {
-                float pos_x = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
-                float pos_z = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
+                float posX = (float)(random.NextDouble() * 2 * MapSize - MapSize);
+                float posZ = (float)(random.NextDouble() * 2 * MapSize - MapSize);
 
-                Tree _newTree = new Tree(Content, new Vector3(pos_x, 0f, pos_z), Matrix.CreateRotationY((float)(_random.NextDouble())), Color.Green);
-                _newTree.LoadContent(Effect);
-                TreeList.Add(_newTree);
+                var newTree = new Tree(Content, new Vector3(posX, 0f, posZ), Matrix.CreateRotationY((float)(random.NextDouble())), Color.Green);
+                newTree.LoadContent(Effect);
+                TreeList.Add(newTree);
             }
         }
 
-        protected void AgregarTunnel(uint _cantidad)
-        {   
-            int seed = 4;
-            Random _random = new Random(seed);
+        protected void AgregarTunnel(uint cantidad)
+        {
+            var random = new Random(4);
 
-            for (uint i = 0; i < _cantidad; i++)
+            for (uint i = 0; i < cantidad; i++)
             {
-                float pos_x = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
-                float pos_z = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
+                float posX = (float)(random.NextDouble() * 2 * MapSize - MapSize);
+                float posZ = (float)(random.NextDouble() * 2 * MapSize - MapSize);
 
-                Tunnel _newTunnel = new Tunnel(Content, new Vector3(pos_x, 0f, pos_z), Matrix.CreateRotationY((float)(_random.NextDouble())), Color.LightYellow);
-                _newTunnel.LoadContent(Effect);
-                TunnelList.Add(_newTunnel);
+                var newTunnel = new Tunnel(Content, new Vector3(posX, 0f, posZ), Matrix.CreateRotationY((float)(random.NextDouble())), Color.LightYellow);
+                newTunnel.LoadContent(Effect);
+                TunnelList.Add(newTunnel);
             }
         }
 
-        protected void AgregarWave_A(uint _cantidad)
-        {   
-            int seed = 6;
-            Random _random = new Random(seed);
+        protected void AgregarWave_A(uint cantidad)
+        {
+            var random = new Random(6);
 
-            for (uint i = 0; i < _cantidad; i++)
+            for (uint i = 0; i < cantidad; i++)
             {
-                float pos_x = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
-                float pos_z = (float)(_random.NextDouble() * 2 * MapSize - MapSize);
+                float posX = (float)(random.NextDouble() * 2 * MapSize - MapSize);
+                float posZ = (float)(random.NextDouble() * 2 * MapSize - MapSize);
 
-                Wave_A _newWave = new Wave_A(Content, new Vector3(pos_x, -3f, pos_z), Matrix.CreateRotationY((float)(_random.NextDouble())), Color.SaddleBrown);
-                _newWave.LoadContent(Effect);
-                Wave_AList.Add(_newWave);
+                var newWave = new Wave_A(Content, new Vector3(posX, -3f, posZ), Matrix.CreateRotationY((float)(random.NextDouble())), Color.SaddleBrown);
+                newWave.LoadContent(Effect);
+                Wave_AList.Add(newWave);
             }
         }
-        
 
-        /// <summary>
-        ///     Libero los recursos que se cargaron en el juego.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // Libero los recursos.
             Content.Unload();
-
             base.UnloadContent();
         }
     }
